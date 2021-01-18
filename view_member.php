336999ef05aa34c74b9461a63fd221f5fccbc15b
1872043_DindaAyuFebriani <71894178+DindaAF@users.noreply.github.com>
2021-01-18 07:48:23 +0700

    view member terbaru

--- view_member.php
+++ view_member.php
@@ -4,12 +4,6 @@
         <th>Id Member</th>
         <th>Plat Motor</th>
         <th>Plat Mobil</th>
-        <?php if($_SESSION['isLoggedIn'] == TRUE) {
-        ?>
-        <th>Action</th>
-        <?php 
-        } 
-    ?>
     </tr>
     </thead>
     <tbody>
@@ -19,10 +13,6 @@
             echo '<td>' . $row->getId_member() . '</td>';
             echo '<td>' . $row->getPlat_motor() . '</td>';
             echo '<td>' . $row->getPlat_mobil() . '</td>';
-            if($_SESSION['isLoggedIn'] == TRUE) {
-            echo '<td><button class="btn btn--radius-2 btn--blue" onclick="updateMember(\''.$row->getId_member().'\')">Update</button>
-            <button class="btn btn--radius-2 btn--blue" onclick="deleteMember(\''.$row->getId_member().'\')">Delete</button></td>';
-            }
             echo '</tr>';
         }
         $link = null;
